fb537ad3d12ddd4f5641a07f21dec1c0989490b1
James Dressel <ubergeek@jamesdressel.com>
2014-04-26 16:24:08 -0400

    Remove unneeded includes

--- src/SoundHandler.cpp
+++ src/SoundHandler.cpp
@@ -1,20 +1,12 @@
 #include "SoundHandler.h"
 
 // Using the standard output for printf 
+#pragma once
 #include <stdio.h>
 #include <stdlib.h>
-#include <vector>
-// Use glew.h instead of gl.h to get all the GL prototypes declared 
-#include <glew.h>
-// Using the SDL library for the base windowing setup 
-#include <SDL/SDL.h>
-#include <SDL/SDL_opengl.h>
-#include <SDL/SDL_thread.h>
-#include <SDL/SDL_timer.h>
+#include <iostream>
+
 #include <SDL/SDL_mixer.h>
-
-
-
 
 
 
