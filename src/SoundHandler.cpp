dcba251d57d34bc9ec033ebf9ec37aae62d0efd1
James Dressel <ubergeek@jamesdressel.com>
2014-04-26 16:26:33 -0400

    Move includes to the header file
    You know, where I should have put them in the first place.

--- src/SoundHandler.cpp
+++ src/SoundHandler.cpp
@@ -1,22 +1,10 @@
 #include "SoundHandler.h"
 
-// Using the standard output for printf 
-#pragma once
-#include <stdio.h>
-#include <stdlib.h>
-#include <iostream>
-
-#include <SDL/SDL_mixer.h>
-
-
-
-
 SoundHandler::SoundHandler(void)
 {
 	std::cout << "SOUND" << std::endl;
 	std::cout << "SOUND" << std::endl;
 }
-
 
 SoundHandler::~SoundHandler(void)
 {
--- src/SoundHandler.h
+++ src/SoundHandler.h
@@ -1,4 +1,12 @@
 #pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+#include <SDL/SDL_mixer.h>
+
+
 class SoundHandler
 {
 public:
