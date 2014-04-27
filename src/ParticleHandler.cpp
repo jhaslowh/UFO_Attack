faef001c503f6c04bf2de4a09c140466c60d963d
Jonathan Haslow-Hall <Jhaslowh@gmail.com>
2014-04-27 16:43:14 -0400

    Particle object and handler created

---
+++ src/ParticleHandler.cpp
@@ -0,0 +1,62 @@
+#include "ParticleHandler.h"
+
+
+ParticleHandler::ParticleHandler()
+{
+	size = 200;
+	parts = new Particle[size];
+}
+
+ParticleHandler::~ParticleHandler()
+{
+	delete[] parts;
+	parts = NULL;
+}
+
+// Returns the particle list
+Particle* ParticleHandler::getList(){
+	return parts;
+}
+
+// Return size
+int ParticleHandler::getSize(){
+	return size;
+}
+
+// Add new particle to list of particle 
+Particle* ParticleHandler::add(int image, float x, float y, float originX, float originY, 
+	float direcX, float direcY, float speed, float lifeTime){
+	return add(image,x,y,originX,originY,direcX,direcY,speed,lifeTime,0.0f,0.0f);
+}
+
+// Add new particle to list of particle 
+Particle* ParticleHandler::add(int image, float x, float y, float originX, float originY, 
+	float direcX, float direcY, float speed, float lifeTime, float rotationSpeed, float scaleSpeed){
+	for (int i = 0; i < size; i++){
+		if (!parts[i].getValid()){
+			parts[i].reset();
+			parts[i].setImageId(image);
+			parts[i].setLoc(x,y);
+			parts[i].setOrigin(originX, originY);
+			parts[i].setDirec(direcX, direcY);
+			parts[i].setSpeed(speed);
+			parts[i].setLifeTime(lifeTime);
+			parts[i].setRotSpeed(rotationSpeed);
+			parts[i].setScaleSpeed(scaleSpeed);
+			return &parts[i];
+		}
+	}
+	return NULL;
+}
+
+// Update current particle states 
+void ParticleHandler::updateProjectiles(float deltaTime){
+	for (int i = 0; i < size; i++)
+		parts[i].update(deltaTime);
+}
+
+// Draw all particles 
+void ParticleHandler::draw(GLHandler* mgl, GameAtlas* mAtlas){
+	for (int i = 0; i < size; i++)
+		parts[i].draw(mgl,mAtlas);
+}
---
+++ src/ParticleHandler.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "Particle.h"
+
+class ParticleHandler
+{
+	private:
+	// Current size of list
+	int size;
+
+	// List of particles 
+	Particle* parts;
+
+public:
+	ParticleHandler();
+	~ParticleHandler();
+
+	// Returns the particle list
+	Particle* getList();
+
+	// Return size
+	int getSize();
+
+	// Add new particle to list of particle 
+	Particle* add(int image, float x, float y, float originX, float originY, 
+		float direcX, float direcY, float speed, float lifeTime);
+	// Add new particle to list of particle 
+	Particle* add(int image, float x, float y, float originX, float originY, 
+		float direcX, float direcY, float speed, float lifeTime, 
+		float rotationSpeed, float scaleSpeed);
+	
+	// Update current particle states 
+	void updateProjectiles(float deltaTime);
+
+	// Draw all particles 
+	void draw(GLHandler* mgl, GameAtlas* mAtlas);
+};
+
